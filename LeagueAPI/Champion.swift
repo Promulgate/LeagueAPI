b11a9fd9c21abd130867d20e7753a8e5e3663842
Eric Chang <echang25@hotmail.com>
2016-12-28 03:17:44 -0500

    repush

---
+++ LeagueAPI/Champion.swift
@@ -0,0 +1,25 @@
+//
+//  Champion.swift
+//  LeagueAPI
+//
+//  Created by Eric Chang on 12/27/16.
+//  Copyright © 2016 Eric Chang. All rights reserved.
+//
+
+import Foundation
+
+//class Champion {
+//    let name: String
+//    let id: Int
+//    let title: String
+//    
+//    init(name: String, id: Int, title: String) {
+//        self.name = name
+//        self.id = id
+//        self.title = title
+//    }
+//    
+//    
+//    
+//    
+//}
---
+++ LeagueAPI/ProjectScenes.swift
@@ -0,0 +1,52 @@
+//
+//  ProjectScenes.swift
+//  LeagueAPI
+//
+//  Created by Eric Chang on 12/27/16.
+//  Copyright © 2016 Eric Chang. All rights reserved.
+//
+
+import Foundation
+
+/**************
+  *
+  *
+  * Scenes, Ideas, Etc.
+  *
+  *
+  **********************/
+
+/*
+ 
+ Nav Control Bar - hidden
+ 
+ Home Scene: Collection View
+    - List champs with square icons
+    - Hamburger for options (Top Left)
+    - Search bar for champions (Top Right)
+ 
+ 
+ 
+ 
+ Functions:
+    - Champ segue to detail
+       > Riot general details
+       > Mechanics/Interactions
+       > Strategies/Tips
+       > Back?/Home? button (Top Right?)
+       > Top button (Position fixed bottom right during scroll -or- Bottom Right)
+ 
+ 
+ 
+ ??:
+    - Core Data?
+    - Some POST function - player notes? home scene?
+    - Enter champ player is laning against for specific matchup strategies - API?
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
